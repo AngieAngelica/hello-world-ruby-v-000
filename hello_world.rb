25dd9ae23928a3ec9e9ac645096bd870b13e3382
Angie Kassab <anasab12@gmail.com>
2018-10-18 02:31:03 +0000

    automatically backed up by learn

--- hello_world.rb
+++ hello_world.rb
@@ -1,1 +1,11 @@
-puts "Hello World!"
+require "nokogiri"
+require "open-uri"
+require "pry"
+
+html = open("http://www.flatironschool.com")
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+doc.css(".grey-text")
+
+puts doc
+binding.pry
+puts "bye"
