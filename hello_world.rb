751fe45638c44e5a46304389f3f07a5d464cd6c2
Angie Kassab <anasab12@gmail.com>
2018-10-18 02:31:42 +0000

    automatically backed up by learn

--- hello_world.rb
+++ hello_world.rb
@@ -3,7 +3,7 @@ require "open-uri"
 require "pry"
 
 html = open("http://www.flatironschool.com")
-doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+doc = Nokogiri::HTML(open("http://timeout.com/"))
 doc.css(".grey-text")
 
 puts doc
